2b76fe791a43a837a4a9f261295cb1ab14a045f3
Fiker-Mesfin <fikermesfin999@gmail.com>
2026-05-22 21:11:27 +0300

    Implemented DFS maze generation with stack-based backtracking

--- Running-Maze.py
+++ Running-Maze.py
@@ -62,6 +62,39 @@ def draw_maze():
             glVertex2f(c * CELL_SIZE, 0)
             glVertex2f((c + 1) * CELL_SIZE, 0)
     glEnd()
+# --- MAZE GENERATION ALGORITHM (DFS) ---
+# Phase 2: Implementing DFS-based maze generation
+def generate_step():
+    global current_cell, generating, solving
+    r, c = current_cell
+    neighbors = []
+    if r < R - 1 and not visited[r + 1][c]: neighbors.append((r + 1, c, 'N'))
+    if r > 0 and not visited[r - 1][c]: neighbors.append((r - 1, c, 'S'))
+    if c < C - 1 and not visited[r][c + 1]: neighbors.append((r, c + 1, 'E'))
+    if c > 0 and not visited[r][c - 1]: neighbors.append((r, c - 1, 'W'))
+
+    if neighbors:
+        nr, nc, d = random.choice(neighbors)
+        stack.append(current_cell)
+        if d == 'N': northWall[r][c] = 0
+        elif d == 'S': northWall[r - 1][c] = 0
+        elif d == 'E': eastWall[r][c] = 0
+        elif d == 'W': eastWall[r][c - 1] = 0
+        visited[nr][nc] = True
+        current_cell = (nr, nc)
+    elif stack:
+        current_cell = stack.pop()
+    else:
+        generating = False
+        eastWall[R - 1][0] = 0
+        eastWall[0][C - 1] = 0 
+        if ENABLE_CYCLES:
+            for _ in range((R * C) // 20):
+                r_rand = random.randint(0, R - 2)
+                c_rand = random.randint(0, C - 2)
+                if random.choice([True, False]): northWall[r_rand][c_rand] = 0
+                else: eastWall[r_rand][c_rand] = 0
+        solving = True
 
 
 
@@ -70,7 +103,13 @@ def display():
     glClear(GL_COLOR_BUFFER_BIT)
     draw_maze()
     draw_entities()
-    glutSwapBuffers() 
+    glutSwapBuffers()
+    
+def update(value):
+    if generating: generate_step()
+    elif solving: solve_step()
+    glutPostRedisplay()
+    glutTimerFunc(20, update, 0)
 
 
 
